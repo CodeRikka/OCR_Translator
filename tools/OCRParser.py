c67bac9a1fc250453fd33a20dc923e8b79ad294f
Zaire <3147879462@qq.com>
2023-05-19 14:29:30 +0800

    UPDATE

--- tools/OCRParser.py
+++ tools/OCRParser.py
@@ -7,13 +7,13 @@ def makeparse():
         "-r0",
         "--root0",
         type=str,
-        default="./pics/example2.jpg",
+        default="./pics/example4.jpg",
     )
     parser.add_argument(
         "-r1",
         "--root1",
         type=str,
-        default="./pics/example2.jpg",
+        default="./pics/example4.jpg",
     )
     parser.add_argument(
         "-o",
