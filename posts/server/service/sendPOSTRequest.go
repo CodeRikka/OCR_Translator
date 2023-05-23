3d5698911d0692d2b88bf5bb86ed52310bf5edc3
Zaire <3147879462@qq.com>
2023-05-23 21:19:43 +0800

    fix some bugs

--- posts/server/service/sendPOSTRequest.go
+++ posts/server/service/sendPOSTRequest.go
@@ -31,7 +31,7 @@ func SendPostRequest(urlStr string, postData map[string]string) ([]byte, error) 
 		formData.WriteString(fmt.Sprintf("%s=%s&", key, value))
 	}
 	formDataString := formData.String()
-
+	fmt.Println(formData)
 	// 创建HTTP请求
 	req, err := http.NewRequest("POST", urlStr, bytes.NewBufferString(formDataString))
 	if err != nil {
@@ -57,14 +57,17 @@ func SendPostRequest(urlStr string, postData map[string]string) ([]byte, error) 
 
 	return body, nil
 }
-func SolvePOST(urlStr string, root0 string, root1 string, tl string, tb string, output string) error {
+func SolvePOST(urlStr string, root0 string, root1 string, tl string, tb string, font_size string, dewarp string, llama string, output string) error {
 	// 构造POST请求参数
 	postData := map[string]string{
-		"root0":  root0,
-		"root1":  root1,
-		"tl":     tl,
-		"tb":     tb,
-		"output": output,
+		"root0":       root0,
+		"root1":       root1,
+		"thresh_line": tl,
+		"thresh_box":  tb,
+		"font_size":   font_size,
+		"dewarp":      dewarp,
+		"llama":       llama,
+		"output":      output,
 	}
 	// 发送HTTP POST请求
 	response, err := SendPostRequest(urlStr, postData)
